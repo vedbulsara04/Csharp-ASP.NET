cb3c0299fc0e9f41dd5d58c2eb5bee3dd7453f0c
Ved <vedbulsara7@gmail.com>
2023-05-13 22:26:51 +0530

    commit 2.1 (13/5/23)

---
+++ Console_Apps/Nested_If/Nested_If/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Reading the username.
+            Console.WriteLine("Enter your username : ");
+            string uname = Console.ReadLine();
+            Console.WriteLine();
+
+            // Reading the password.
+            Console.WriteLine("Enter your password : ");
+            string pass = Console.ReadLine();
+            Console.WriteLine();
+
+            /*Nested-if Conditional statement to check username & password*/
+            if(uname == "vedbulsara04")
+            {
+                if(pass == "ved123")
+                {
+                    Console.WriteLine("Login Success!");
+                }
+            }                                           
+            Console.WriteLine("Login unsuccessful");
+            Console.ReadKey();
+        }
+    }
+}
