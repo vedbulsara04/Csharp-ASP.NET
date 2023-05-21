f436fa1830b77120f0a9958cfc7192054d297b59
Ved <vedbulsara7@gmail.com>
2023-05-21 22:38:44 +0530

    Class Library (21/5/23)

---
+++ Console_Apps/Class_Library_Projects/TestLibrary/TestLibrary/Program.cs
@@ -0,0 +1,18 @@
+﻿using UtilityLibraries;
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter the String.(empty/null to end)");
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                break;
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("Begins with uppercase? " +
+                 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+            Console.WriteLine();
+        }
+    }
+}
