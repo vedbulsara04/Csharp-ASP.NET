10c33e6f80f673eefa2999ff3cf82ca0fb7d5c29
Ved <vedbulsara7@gmail.com>
2023-05-21 20:50:44 +0530

    For Loop (21/5/23)

---
+++ Console_Apps/For_Loop_2 (multiple_iterators)/For_Loop_2 (multiple_iterators)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_2__multiple_iterators_
+{
+    class ForLoop2
+    {
+        public static void Main(string[] args)
+        {
+            for (int i=0, j=0; i+j<=7; i++, j++)
+            {
+                Console.WriteLine("i = {0} and j = {1}", i,j);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Console_Apps/For_Loop_3 (foreach loop)/For_Loop_3 (foreach loop)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop_3__foreach_loop_
+{
+    class ForEach
+    {
+        public static void Main(string[] args)
+        {
+            string[] formula1 = {"Mercedes AMG Petronas Formula1 team", "Scuderria Ferrari Fromula1 team", "Oracle Red-Bull racing Formula1 team"};
+
+            foreach (string team in formula1)
+            {
+                Console.WriteLine(team);    
+            }
+            
+            Console.ReadKey();
+        }
+
+    }
+}
